901efc9bdf81cd1e61e5ccac01a5136413c033a6
Carsten Schnober <carschno@gmail.com>
2020-10-05 13:16:10 +0200

    Add package_dir, package_date

--- setup.py
+++ setup.py
@@ -11,15 +11,17 @@ setuptools.setup(
     description="API for The Syllabus modules classifier.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(where="./src", exclude=("tests",)),
+    package_dir={"": str("src")},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    package_data={"module_classifier": ["data/classifier.model.ftz"]},
+    include_package_data=True,
     python_requires='>=3.8',
     # conda install -c conda-forge fasttext
     install_requires=["fasttext==0.9.2"],
-    test_requires=["pytest-subtests==0.3.2", "pytest-mock==3.3.1"],
+    # test_requires=["pytest-subtests==0.3.2", "pytest-mock==3.3.1"],
 )
