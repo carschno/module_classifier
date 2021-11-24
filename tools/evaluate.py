54e996dbc8a73462674a17e29508d99e31a4aee9
Carsten Schnober <carschno@gmail.com>
2021-11-24 16:06:42 +0100

    Add --labels flag, --delimiter argument.

--- tools/evaluate.py
+++ tools/evaluate.py
@@ -2,6 +2,7 @@
 
 import argparse
 
+from module_classifier.settings import DEFAULT_MODULE_DELIMITER
 from module_trainer.training import Trainer
 
 if __name__ == "__main__":
@@ -22,16 +23,41 @@ if __name__ == "__main__":
         help="The model file.",
     )
 
+    parser.add_argument(
+        "--delimiter",
+        "-d",
+        type=str,
+        required=False,
+        help=f"The delimiter used in module labels as in 'S1.M1' or 'S1_M1'. If not given, uses default ('{DEFAULT_MODULE_DELIMITER}').",
+    )
+
+    parser.add_argument(
+        "--labels", "-l", action="store_true", help="Print model labels only."
+    )
+
     parser.add_argument("-k", type=int, default=1)
     parser.add_argument("--threshold", "-t", type=float, default=0.0)
 
     args = parser.parse_args()
 
-    trainer = Trainer()
-    n, precision, recall = trainer.evaluate_model(
-        args.input.name, args.model_file.name, k=args.k, threshold=args.threshold
-    )
+    if args.labels:
+        from fasttext import FastText
 
-    print(f"Number of samples:\t{n}")
-    print(f"Precision:\t{precision}")
-    print(f"Recall:\t{recall}")
+        model = FastText.load_model(args.model_file.name)
+        labels, frequencies = model.get_labels(include_freq=True)
+        for l, f in zip(labels, frequencies):
+            print(f"{l}\t{f}")
+
+    else:
+        trainer = Trainer()
+        n, precision, recall = trainer.evaluate_model(
+            args.input.name,
+            args.model_file.name,
+            k=args.k,
+            threshold=args.threshold,
+            module_delimiter=args.delimiter,
+        )
+
+        print(f"Number of samples:\t{n}")
+        print(f"Precision:\t{precision}")
+        print(f"Recall:\t{recall}")
