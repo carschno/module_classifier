6eb7241d72e385a9f114674a45f0d95e7f073cb7
Carsten Schnober <carschno@gmail.com>
2021-12-08 14:49:50 +0100

    Add test_predict_texts() and test_predict_rows().

--- tests/integration/test_classifier.py
+++ tests/integration/test_classifier.py
@@ -1,5 +1,6 @@
+import numpy as np
 import pytest
-from src.module_classifier.classification import Classifier, Prediction
+from src.module_classifier.classification import Classifier, Prediction, Predictions
 from src.module_classifier.preprocessing import Module
 
 from ..conftest import TEST_MODEL, does_not_raise
@@ -7,8 +8,10 @@ from ..conftest import TEST_MODEL, does_not_raise
 
 @pytest.mark.skipif(TEST_MODEL is None, reason="'TEST_MODEL' not specified.")
 class TestClassifier:
+    classifier = Classifier(TEST_MODEL)
+
     def test_init(self):
-        assert isinstance(Classifier(TEST_MODEL), Classifier)
+        assert isinstance(self.classifier, Classifier)
 
     @pytest.mark.parametrize(
         "row,columns,k,expected",
@@ -68,8 +71,34 @@ class TestClassifier:
         ids=["empty fields", "test fields", "k=2"],
     )
     def test_predict_row(self, row, columns, k, expected):
-        model = Classifier(TEST_MODEL)
-        assert model.predict_row(row, k, columns=columns) == expected
+        assert self.classifier.predict_row(row, k, columns=columns) == expected
+
+    @pytest.mark.parametrize(
+        "rows, columns, k, expected",
+        [
+            ([], [], 1, []),
+            (
+                [
+                    {"text": "a text about China"},
+                    {"text": "something related to artificial intelligence"},
+                ],
+                ["text"],
+                1,
+                [
+                    Predictions(
+                        labels=["__label__S5_M8"],
+                        probs=np.array([0.99993765], dtype=np.float32),
+                    ),
+                    Predictions(
+                        labels=["__label__S6_M8"],
+                        probs=np.array([1.0000085], dtype=np.float32),
+                    ),
+                ],
+            ),
+        ],
+    )
+    def test_predict_rows(self, rows, columns, k, expected):
+        assert self.classifier.predict_rows(rows, k, columns=columns) == expected
 
     @pytest.mark.parametrize(
         "text,k,expected",
@@ -87,8 +116,32 @@ class TestClassifier:
         ids=["AI & Automation"],
     )
     def test_predict_text(self, text, k, expected):
-        model = Classifier(TEST_MODEL)
-        assert model.predict_text(text, k) == expected
+        assert self.classifier.predict_text(text, k) == expected
+
+    @pytest.mark.parametrize(
+        "texts,k,expected",
+        [
+            ([], 3, []),
+            (
+                ["text about china", "ai automation"],
+                3,
+                [
+                    Predictions(
+                        labels=["__label__S5_M8", "__label__S6_M9", "__label__S6_M8"],
+                        probs=np.array([9.9993765e-01, 7.7751087e-05, 1.4639512e-05]),
+                    ),
+                    Predictions(
+                        labels=["__label__S6_M8", "__label__S4_M5", "__label__S4_M2"],
+                        probs=np.array([1.0000100e00, 1.0000003e-05, 1.0000003e-05]),
+                    ),
+                ],
+            ),
+        ],
+    )
+    def test_predict_texts(self, texts, k, expected):
+        for prediction, e in zip(self.classifier.predict_texts(texts, k), expected):
+            assert prediction.labels == e.labels
+            assert np.allclose(prediction.probs, e.probs)
 
     @pytest.mark.parametrize(
         "remote,local,expected_exception",
@@ -103,9 +156,8 @@ class TestClassifier:
             assert isinstance(Classifier.download(remote, local), Classifier)
 
     def test_labels(self):
-        assert len(Classifier(TEST_MODEL).labels) == 60
+        assert len(self.classifier.labels) == 60
 
     def test_modules(self):
-        model = Classifier(TEST_MODEL)
-        assert len(model.modules) == 60
-        assert all(isinstance(m, Module) for m in model.modules)
+        assert len(self.classifier.modules) == 60
+        assert all(isinstance(m, Module) for m in self.classifier.modules)
